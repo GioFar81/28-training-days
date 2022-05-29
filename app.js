d8a50607cce7518b57f4dc682bb1d72f8bf702ca
GioFar <farigiovanni@gmail.com>
2022-05-29 22:49:14 +0100

    Version 01, initial state.

---
+++ app.js
@@ -0,0 +1,48 @@
+// GET A RANDOM EVENT
+const getRandEvent = () => {
+    const random = Math.floor(Math.random() * 3);
+    if (random === 0) {
+        return 'Marathon';
+    } else if (random === 1) {
+        return 'Triathlon';
+    } else if (random === 2) {
+        return 'Pentathlon';
+    }
+};
+
+// ASSIGN A NUMBER OF DAYS BASED ON THE RESULT OF THE FUNCTION ABOVE
+const getTrainingDays = event => {
+    let days;
+    if (event === 'Marathon') {
+        days = 50;
+    } else if (event === 'Triathlon') {
+        days = 100;
+    } else if (event === 'Pentathlon') {
+        days = 200;
+    }
+    return days;
+};
+
+const name = 'Nala';
+
+const logEvent = (name, event) => {
+    console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+    console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+
+logEvent(name, event);
+logTime(name, days);
+
+
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+
+logEvent(name2, event2);
+logTime(name2, days2);
